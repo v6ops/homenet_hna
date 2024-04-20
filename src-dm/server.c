89c3592e4573a4f01b24a2a61dc60e527d7473e4
v6ops <v6ops@globis.net>
2024-04-20 09:26:11 +0200

    Initial add of multi-threaded ssl libevent server in src-dm

---
+++ src-dm/server.c
@@ -0,0 +1,10 @@
+#include "libevent-cb.h"
+#include "workqueue.h"
+/**
+ *  Main function for demonstrating the echo server.
+ *  You can remove this and simply call runServer() from your application. 
+ */
+int main(int argc, char *argv[]) {
+    int port = argc > 1 && atoi(argv[1]) > 0 ? : DEFULT_SERVER_PORT;
+    return runServer(port);
+}
